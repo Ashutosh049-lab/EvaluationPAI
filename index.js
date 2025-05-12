e05032fba0be8631af8b1c4e2dfb363845ee2c0b
Ashutoshkj <jashutosh528@gmail.com>
2025-05-12 21:30:43 +0530

    Problem 2: Vehicle and Car Constructor (Inheritance)

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 
 
-console.log("hello")
+  // problem -2
+
+  function Vehicle(make,year){
+    this.make=make;
+    this.year=year;
+  }
+
+  Vehicle.prototype.getDetails=function(){
+    return `${this.make}  (${this.year})`;
+  }
+
+
+  function Car(make,year,model){
+    Vehicle.call(this,make,year);
+    this.model=model;
+  }
+
+
+  Car.prototype=Object.create(Vehicle.prototype);
+  Car.prototype.constructor=Car;
+
+
+  // override
+  Car.prototype.getDetails=function(){
+    return `${this.make} ${this.model} (${this.year})`;
+  }
+
+  const car1 = new Car("Toyota", 2022, "Corolla");
+console.log(car1.getDetails());
